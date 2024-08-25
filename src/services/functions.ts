ef13d6133964e64bb57bce54b5cb1a6fa38e8c4a
Ivan <ivanglzr01@gmail.com>
2024-08-26 00:26:27 +0200

    functions to validate petitions

---
+++ src/services/functions.ts
@@ -0,0 +1,29 @@
+import { BackendResponseInterface } from "@/interfaces/responses";
+
+import {
+  ForbiddenError,
+  UnauthorizedError,
+  StatusError,
+} from "@/errors/errors";
+
+export function validateResponse(
+  res: BackendResponseInterface,
+  statusCode: number
+) {
+  if (statusCode === 403) throw new ForbiddenError(res.message);
+  if (statusCode === 401) throw new UnauthorizedError(res.message);
+  if (res.status === "error") throw new StatusError(res.message);
+}
+
+export function handleErrors(error: unknown) {
+  if (error instanceof ForbiddenError) {
+    window.location.href = "/forbidden";
+    throw error;
+  }
+  if (error instanceof UnauthorizedError) {
+    window.location.href = "/login";
+    throw error;
+  }
+
+  throw error;
+}
