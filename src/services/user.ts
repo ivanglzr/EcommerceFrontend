8f5195a7f7c7c158ef44e34828e740d4da012b9a
Ivan <ivanglzr01@gmail.com>
2024-08-26 00:28:38 +0200

    login user function created with validating functions

---
+++ src/services/user.ts
@@ -0,0 +1,25 @@
+import { ROUTES } from "./constants";
+
+import { BackendResponseInterface } from "@/interfaces/responses";
+import { LogInInterface } from "@/interfaces/user";
+
+import { handleErrors, validateResponse } from "./functions";
+
+export async function loginUser(
+  loginData: LogInInterface
+): Promise<BackendResponseInterface> {
+  try {
+    const petition = await fetch(ROUTES.URL + ROUTES.LOGIN_USER, {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify(loginData),
+    });
+    const res: BackendResponseInterface = await petition.json();
+
+    validateResponse(res, petition.status);
+
+    return res;
+  } catch (error) {
+    throw handleErrors(error);
+  }
+}
